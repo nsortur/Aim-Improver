a57d94d2460905480fc77d3912f9bc92fb57f09b
NeelS <nsortur@yahoo.com>
2019-08-30 18:14:22 -0400

    at least displays

--- src/App.vue
+++ src/App.vue
@@ -1,155 +1,180 @@
 1<template>
-    <div class=container>
-        <div class="resultBox">
-            <div></div>
-        </div>
-        <div>
-            <button type="button" class="buttons" id="one">1</button>
-            <button type="button" class="buttons" id="two">2</button>
-            <button type="button" class="buttons" id="three">3</button>
-            <button type="button" class="buttons" id="four">4</button>
-            <button type="button" class="buttons" id="five">5</button>
-            <button type="button" class="buttons" id="six">6</button>
-            <button type="button" class="buttons" id="seven">7</button>
-            <button type="button" class="buttons" id="eight">8</button>
-            <button type="button" class="buttons" id="nine">9</button>
-            <button type="button" class="buttons" id="zero">0</button>
-            <button type="button" class="buttons" id="equals">=</button>
-        </div>
-        <div>
-            <button type="button" class="buttons" id="divide">/</button>
-            <button type="button" class="buttons" id="mult">X</button>
-            <button type="button" class="buttons" id="minus">-</button>
-            <button type="button" class="buttons" id="plus">+</button>
-            <button type="button" class="buttons" id="clear">CLEAR</button>
-        </div>
+  <div class="container">
+    <div id="resultBox"></div>
+    <div>
+      <button type="button" class="buttons" id="one" @click="display(1)">1</button>
+      <button type="button" class="buttons" id="two" @click="display(2)">2</button>
+      <button type="button" class="buttons" id="three" @click="display(3)">3</button>
+      <button type="button" class="buttons" id="four" @click="display(4)">4</button>
+      <button type="button" class="buttons" id="five" @click="display(5)">5</button>
+      <button type="button" class="buttons" id="six" @click="display(6)">6</button>
+      <button type="button" class="buttons" id="seven" @click="display(7)">7</button>
+      <button type="button" class="buttons" id="eight" @click="display(8)">8</button>
+      <button type="button" class="buttons" id="nine" @click="display(9)">9</button>
+      <button type="button" class="buttons" id="zero" @click="display(0)">0</button>
+      <button type="button" class="buttons" id="equals">=</button>
     </div>
+    <div>
+      <button type="button" class="buttons" id="divide" @click="calculate('divide')">/</button>
+      <button type="button" class="buttons" id="mult" @click="calculate('multiply')">X</button>
+      <button type="button" class="buttons" id="minus" @click="calculate('subtract')">-</button>
+      <button type="button" class="buttons" id="plus" @click="calculate('add')">+</button>
+      <button type="button" class="buttons" id="clear" @click="display('')">CLEAR</button>
+    </div>
+  </div>
 </template>
 
 <script>
-
 export default {
-  name: 'vue-app',
-  
-}
+  name: "vue-app",
+  data() {
+    return {
+      firstVal: 0,
+      secondVal: 0,
+      result: 0
+    };
+  },
+  methods: {
+    calculate(event) {
+      if (event == "divide") {
+        this.result = this.firstVal / this.secondVal;
+      } else if (event == "multiply") {
+        this.result = this.firstVal * this.econdVal;
+      } else if (event == "add") {
+        this.result = this.firstVal + this.secondVal;
+      } else if (event == "subtract") {
+        this.result = this.firstVal - this.secondVal;
+      } else {
+        console.log("wat");
+      }
+    },
+    display(event) {
+      (document.getElementById("resultBox").innerHTML = event);
+    }
+  }
+};
 </script>
 
 <style>
-.container{
-    position:absolute;
-    width: 300px;
-    height: 600px;
-    border: 10px solid blue;
-    padding: 200px;
+.container {
+  position: absolute;
+  width: 300px;
+  height: 600px;
+  border: 10px solid blue;
+  padding: 200px;
 }
-.resultBox{
-    position:absolute;
-    top:0px;
-    left:0px;
-    border: 10px solid black;
-    width: 400px;
-    height:100px;
-    background:transparent;
-    background-color:antiquewhite;
-    
+#resultBox {
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  border: 10px solid black;
+  width: 400px;
+  height: 100px;
+  background: transparent;
+  background-color: antiquewhite;
+  font-size: 45px;
+  color: black;
+  padding-right: 10px;
+  text-align: right;
 }
-.buttons{
-    padding:10px;
-    width:100px;
-    height: 100px;
-    border:5px solid black;
+.buttons {
+  padding: 10px;
+  width: 100px;
+  height: 100px;
+  border: 5px solid black;
+  font-size: 35px;
 }
-.buttons:hover{
-    background-color: grey;
-    cursor:pointer;
+.buttons:hover {
+  background-color: grey;
+  cursor: pointer;
 }
-#one{
-    position:absolute;
-    top:300px;
-    left:0px;
+#one {
+  position: absolute;
+  top: 300px;
+  left: 0px;
 }
-#two{
-    position:absolute;
-    top:300px;
-    left:100px;
+#two {
+  position: absolute;
+  top: 300px;
+  left: 100px;
 }
-#three{
-    position:absolute;
-    top:300px;
-    left:200px;
+#three {
+  position: absolute;
+  top: 300px;
+  left: 200px;
 }
-#four{
-    position:absolute;
-    top:200px;
-    left:0px;
+#four {
+  position: absolute;
+  top: 200px;
+  left: 0px;
 }
-#five{
-    position:absolute;
-    top:200px;
-    left:100px;
+#five {
+  position: absolute;
+  top: 200px;
+  left: 100px;
 }
-#six{
-    position:absolute;
-    top:200px;
-    left:200px;
+#six {
+  position: absolute;
+  top: 200px;
+  left: 200px;
 }
-#seven{
-    position:absolute;
-    top:100px;
-    left:0px;
+#seven {
+  position: absolute;
+  top: 100px;
+  left: 0px;
 }
-#eight{
-    position:absolute;
-    top:100px;
-    left:100px;
+#eight {
+  position: absolute;
+  top: 100px;
+  left: 100px;
 }
-#nine{
-    position:absolute;
-    top:100px;
-    left:200px;
+#nine {
+  position: absolute;
+  top: 100px;
+  left: 200px;
 }
-#zero{
-    position:absolute;
-    top:400px;
-    left:0px;
-    width:200px;
+#zero {
+  position: absolute;
+  top: 400px;
+  left: 0px;
+  width: 200px;
 }
-#equals{
-    position:absolute;
-    top:400px;
-    left:200px;
-    width:100px;
+#equals {
+  position: absolute;
+  top: 400px;
+  left: 200px;
+  width: 100px;
 }
-#divide{
-    position:absolute;
-    top:100px;
-    left:300px;
-    width:100px;
+#divide {
+  position: absolute;
+  top: 100px;
+  left: 300px;
+  width: 100px;
 }
-#mult{
-    position:absolute;
-    top:200px;
-    left:300px;
-    width:100px;
+#mult {
+  position: absolute;
+  top: 200px;
+  left: 300px;
+  width: 100px;
 }
-#minus{
-    position:absolute;
-    top:300px;
-    left:300px;
-    width:100px;
+#minus {
+  position: absolute;
+  top: 300px;
+  left: 300px;
+  width: 100px;
 }
-#plus{
-    position:absolute;
-    top:400px;
-    left:300px;
-    width:100px;
+#plus {
+  position: absolute;
+  top: 400px;
+  left: 300px;
+  width: 100px;
 }
-#clear{
-    position:absolute;
-    top:500px;
-    height:80px;
-    left:0px;
-    width:400px;
+#clear {
+  position: absolute;
+  top: 500px;
+  height: 80px;
+  left: 0px;
+  width: 400px;
 }
 </style>
 
